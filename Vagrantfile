a37f528346315e1fd5a1382b536c4f3919e8b6e5
thebarbershopper <cld251@gmail.com>
2015-11-23 15:17:30 -0600

    Install angr in a virtualenv

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,8 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "trusty64"
   config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
-  config.vm.provision :shell, :path => "vagrant_setup.sh"
+  config.vm.provision :shell, :path => "vagrant_setup.sh", :privileged => false
+  config.ssh.username = 'vagrant'
   config.ssh.forward_agent = true
 
   config.vm.synced_folder "host-share", "/home/vagrant/host-share"
--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
 # Updates
-# apt-get -y update
-# apt-get -y upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
-apt-get -y install python3-pip
-apt-get -y install tmux
-apt-get -y install gdb gdb-multiarch
-apt-get -y install unzip
-apt-get -y install foremost
+sudo apt-get -y install python3-pip
+sudo apt-get -y install tmux
+sudo apt-get -y install gdb gdb-multiarch
+sudo apt-get -y install unzip
+sudo apt-get -y install foremost
 
 # QEMU with MIPS/ARM - http://reverseengineering.stackexchange.com/questions/8829/cross-debugging-for-mips-elf-with-qemu-toolchain
-apt-get -y install qemu qemu-user qemu-user-static
-apt-get -y install 'binfmt*'
-apt-get -y install libc6-armhf-armel-cross
-apt-get -y install debian-keyring
-apt-get -y install debian-archive-keyring
-apt-get -y install emdebian-archive-keyring
+sudo apt-get -y install qemu qemu-user qemu-user-static
+sudo apt-get -y install 'binfmt*'
+sudo apt-get -y install libc6-armhf-armel-cross
+sudo apt-get -y install debian-keyring
+sudo apt-get -y install debian-archive-keyring
+sudo apt-get -y install emdebian-archive-keyring
 tee /etc/apt/sources.list.d/emdebian.list << EOF
 deb http://mirrors.mit.edu/debian squeeze main
 deb http://www.emdebian.org/debian squeeze main
 EOF
-apt-get -y install libc6-mipsel-cross
-apt-get -y install libc6-arm-cross
+sudo apt-get -y install libc6-mipsel-cross
+sudo apt-get -y install libc6-arm-cross
 mkdir /etc/qemu-binfmt
 ln -s /usr/mipsel-linux-gnu /etc/qemu-binfmt/mipsel 
 ln -s /usr/arm-linux-gnueabihf /etc/qemu-binfmt/arm
 rm /etc/apt/sources.list.d/emdebian.list
-# apt-get update
+# sudo apt-get update
 
 # Install Binjitsu
-apt-get -y install python2.7 python-pip python-dev git
-pip install --upgrade git+https://github.com/binjitsu/binjitsu.git
+sudo apt-get -y install python2.7 python-pip python-dev git
+sudo pip install --upgrade git+https://github.com/binjitsu/binjitsu.git
 
+cd
 mkdir tools
 cd tools
 
 # Install pwndbg
 git clone https://github.com/zachriggle/pwndbg
-echo "source $PWD/pwndbg/gdbinit.py" >> ~/.gdbinit
+echo source `pwd`/pwndbg/gdbinit.py >> ~/.gdbinit
 
 # Capstone for pwndbg
 git clone https://github.com/aquynh/capstone
@@ -49,7 +50,7 @@ cd bindings/python
 sudo python3 setup.py install # Ubuntu 14.04+, GDB uses Python3
 
 # pycparser for pwndbg
-pip3 install pycparser # Use pip3 for Python3
+sudo pip3 install pycparser # Use pip3 for Python3
 
 # Install radare2
 git clone https://github.com/radare/radare2
@@ -57,14 +58,14 @@ cd radare2
 ./sys/install.sh
 
 # Install binwalk
-cd /home/vagrant/tools
+cd 
 git clone https://github.com/devttys0/binwalk
 cd binwalk
 sudo python setup.py install
 
 # Install Firmware-Mod-Kit
-apt-get -y install git build-essential zlib1g-dev liblzma-dev python-magic
-cd /home/vagrant/tools
+sudo apt-get -y install git build-essential zlib1g-dev liblzma-dev python-magic
+cd ~/tools
 wget https://firmware-mod-kit.googlecode.com/files/fmk_099.tar.gz
 tar xvf fmk_099.tar.gz
 rm fmk_099.tar.gz
@@ -72,15 +73,25 @@ cd fmk_099/src
 ./configure
 make
 
-# Personal config
-cd /home/vagrant/tools
-git clone https://github.com/thebarbershopper/environment
-cd environment
-cp -r . /home/vagrant
-
 # Uninstall capstone
 sudo pip2 uninstall capstone -y
 
 # Install correct capstone
-cd /home/vagrant/tools/capstone/bindings/python
+cd ~/tools/capstone/bindings/python
 sudo python setup.py install
+
+# Personal config
+sudo sudo apt-get -y install stow
+cd /home/vagrant
+rm .bashrc
+git clone https://github.com/thebarbershopper/dotfiles
+cd dotfiles
+./install.sh
+
+# Install Angr
+cd /home/vagrant
+sudo apt-get -y install python-dev libffi-dev build-essential virtualenvwrapper
+sudo pip install virtualenv
+virtualenv angr
+source angr/bin/activate
+pip install angr --upgrade
